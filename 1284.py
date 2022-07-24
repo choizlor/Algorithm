0ad801b77ca57f874045df1b1119f5d95b59e748
choizlor <choizlor@gmail.com>
2022-07-25 00:32:54 +0900

    1284 완료

---
+++ 1284.py
@@ -0,0 +1,12 @@
+T = int(input())
+for num in range(1, T + 1):
+    p, q, r, s, w = map(int, input().split())
+    a_price = p * w
+    b_price = q
+    if w > r:
+        b_price = q + (w - r) * s
+    if a_price < b_price:
+        result = a_price
+    else:
+        result = b_price
+    print(f'#{num} {result}')

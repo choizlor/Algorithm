e975a398a6054349df0ae96ea0e5fcd7a40cda70
choizlor <choizlor@gmail.com>
2022-09-08 00:49:44 +0900

    0908

---
+++ BOJ/1629_곱셈.py
@@ -0,0 +1,13 @@
+def recursive(a, b):
+    if b == 1:
+        return a % C
+    temp = recursive(a, b//2)
+
+    if b % 2 == 0:
+        return temp * temp % C
+    else:
+        return temp * temp * a % C
+
+
+A, B, C = map(int, input().split())
+print(recursive(A, B))

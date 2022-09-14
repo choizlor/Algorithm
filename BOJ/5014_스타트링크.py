cc606dd24659dee3757639101ffe2c7396c4a12f
choizlor <choizlor@gmail.com>
2022-09-15 01:05:03 +0900

    0915

--- BOJ/5014_스타트링크.py
+++ BOJ/5014_스타트링크.py
@@ -1,1 +1,14 @@
+from collections import deque
+
+def bfs(s):
+    dq = deque()
+    dq.append(s)
+    while dq:
+        sumV = 0
+        m = dq.popleft()
+        while 1 <= sumV <= F:
+            sumV = m + U
+
+# 최단거리처럼 풀면 될듯.
 F, S, G, U, D = map(int, input().split())
+bfs(S)
--- BOJ/9205_맥주 마시면서 걸어가기.py
+++ BOJ/9205_맥주 마시면서 걸어가기.py
@@ -1,4 +1,14 @@
 t = int(input())
+
 for _ in range(t):
     n = int(input())
-    
+    arr = [list(map(int, input().split())) for _ in range(n+2)]
+    # beer = 20
+    result = 'happy'
+
+    for i in range(n+1):
+        if (abs(arr[i+1][0] - arr[i][0]) + abs(arr[i+1][1] - arr[i][1])) / 50 > 20:
+            result = 'sad'
+            break
+
+    print(result)

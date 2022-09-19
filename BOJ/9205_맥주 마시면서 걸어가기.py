e422c89a54756b5269058cf428f1163f7dd02d87
choizlor <choizlor@gmail.com>
2022-09-20 00:41:04 +0900

    0919

--- BOJ/9205_맥주 마시면서 걸어가기.py
+++ BOJ/9205_맥주 마시면서 걸어가기.py
@@ -1,14 +1,31 @@
+from collections import deque
+
+
+def bfs():
+    dq = deque()
+    dq.append((home[0], home[1]))
+    while dq:
+        x, y = dq.popleft()
+        if abs(x - fes[0]) + abs(y - fes[1]) <= 1000:
+            return 'happy'
+        for i in range(n):
+            if not visited[i]:
+                nx, ny = p[i]
+                if abs(x - nx) + abs(y - ny) <= 1000:
+                    dq.append((nx, ny))
+                    visited[i] = 1
+    return 'sad'
+
+
 t = int(input())
 
 for _ in range(t):
     n = int(input())
-    arr = [list(map(int, input().split())) for _ in range(n+2)]
-    # beer = 20
-    result = 'happy'
-
-    for i in range(n+1):
-        if (abs(arr[i+1][0] - arr[i][0]) + abs(arr[i+1][1] - arr[i][1])) / 50 > 20:
-            result = 'sad'
-            break
-
-    print(result)
+    home = list(map(int, input().split()))
+    p = []
+    for _ in range(n):
+        x, y = map(int, input().split())
+        p.append((x, y))
+    fes = list(map(int, input().split()))
+    visited = [0 for _ in range(n)]
+    print(bfs())

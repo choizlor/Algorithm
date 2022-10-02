5f1b8bbec231beac2a55652d82a1b5f31b49160e
choizlor <choizlor@gmail.com>
2022-10-03 01:40:07 +0900

    1003

--- BOJ/15686_치킨 배달.py
+++ BOJ/15686_치킨 배달.py
@@ -1,4 +1,40 @@
+def comb(L, BeginWith):
+    # 종료조건 (depth, level 등)
+    if L == M:
+        xy = result[:]
+        chickenC.append(xy)
+    else:
+        for i in range(BeginWith, len(chicken)):
+            result[L] = chicken[i]
+            comb(L + 1, i + 1)
+
+
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-# bfs 돌리고, 치킨집까지의 거리 min/좌표 구하고, 그 minV 보다 크면 return 해버리기
-# 좌표랑 최솟값 같이 들어있는 list를 보고 분석.
+home = []
+chicken = []
+for x in range(N):
+    for y in range(N):
+        if arr[x][y] == 1:
+            home.append([x,y])
+        elif arr[x][y] == 2:
+            chicken.append([x,y])
+
+result = [False] * M
+chickenC = []
+comb(0,0)
+
+answer = 1000000
+for c in chickenC:
+    sumV = 0
+    for h in home:
+        minV = 1000000
+        for i in range(M):
+            dist = abs(h[0] - c[i][0]) + abs(h[1] - c[i][1])
+            if minV > dist:
+                minV = dist
+        sumV += minV
+    if answer > sumV:
+        answer = sumV
+
+print(answer)

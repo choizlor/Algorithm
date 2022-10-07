e771fbf4441740c5f930cf356436192b39bd931d
최지우 <choizlor@gmail.com>
2022-10-07 17:59:45 +0900

    1007

---
+++ BOJ/1240_노드사이의 거리.py
@@ -0,0 +1,9 @@
+N, M = map(int, input().split())
+G = [[0] * (N+1) for _ in range(N+1)]
+
+for _ in range(N-1):
+    u, v, w = map(int, input().split())
+    G[u][v] = w
+    G[v][u] = w
+
+print(G)

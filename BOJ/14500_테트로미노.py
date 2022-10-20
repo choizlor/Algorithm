67a4600f2ccd5ea686bc3fdc537cb6be85ad5a15
choizlor <choizlor@gmail.com>
2022-10-20 23:52:13 +0900

    1020

---
+++ BOJ/14500_테트로미노.py
@@ -0,0 +1,29 @@
+def dfs(x, y, d, sumV):
+    global maxV
+
+    if d == 4:
+        if maxV < sumV:
+            maxV = sumV
+        return
+
+    for dx, dy in [(1,0),(0,1),(-1,0),(0,-1)]:
+        nx, ny = x + dx, y + dy
+        if 0<=nx<N and 0<=ny<M and visited[nx][ny] == 0:
+            visited[nx][ny] = 1
+            if d + 1 == 3:
+                dfs(x, y, d+1, sumV+num[nx][ny])
+            dfs(nx, ny, d+1, sumV+num[nx][ny])
+            visited[nx][ny] = 0
+
+
+N, M = map(int, input().split())
+num = [list(map(int, input().split())) for _ in range(N)]
+visited = [[0] * M for _ in range(N)]
+maxV = 0
+
+for x in range(N):
+    for y in range(M):
+        visited[x][y] = 1
+        dfs(x, y, 1, num[x][y])
+        visited[x][y] = 0
+print(maxV)
--- BOJ/15683_감시.py
+++
@@ -1,3 +0,0 @@
-N, M = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
-

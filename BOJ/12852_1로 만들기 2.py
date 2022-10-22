0524404527f9743738c118cee96d55a718f4d906
choizlor <choizlor@gmail.com>
2022-10-23 00:17:28 +0900

    1022

--- BOJ/12852_1로 만들기 2.py
+++ BOJ/12852_1로 만들기 2.py
@@ -1,21 +1,25 @@
 def solve(k, n, lst):
-    global minV,
+    global minV, result
+
+    if minV <= k:
+        return
 
     if n == 1:
         if minV > k:
             minV = k
+            result = lst[:]
         return
 
     if n % 3 == 0:
         solve(k+1, n//3, lst+[n//3])
-    elif n % 2 == 0:
+    if n % 2 == 0:
         solve(k+1, n//2, lst+[n//2])
     solve(k+1, n-1, lst+[n-1])
 
 
 n = int(input())
-minV = 100000
-lst_a = [n]
-solve(0, n, lst)
-print(lst)
+minV = 10000000
+result = []
+solve(0, n, [n])
 print(minV)
+print(*result)

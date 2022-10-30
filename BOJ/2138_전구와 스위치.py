268aab793a99c37afef4b15660bbdadba32cf583
choizlor <choizlor@gmail.com>
2022-10-30 23:58:43 +0900

    1030

--- BOJ/2138_전구와 스위치.py
+++ BOJ/2138_전구와 스위치.py
@@ -1,4 +1,19 @@
 N = int(input())
 check = list(map(int, list(input())))
 result = list(map(int, list(input())))
+cnt = 0
 
+for i in range(N):
+        if check[i] != result[i]:
+            if i == 0:
+                check[i] = 1 - check[i]
+                check[i+1] = 1 - check[i+1]
+            elif i == N-1:
+                check[i-1] = 1 - check[i-1]
+                check[i] = 1 - check[i]
+            else:
+                check[i-1] = 1 - check[i-1]
+                check[i] = 1 - check[i]
+                check[i + 1] = 1 - check[i + 1]
+            for x in range(i, i+3):
+                check[x] = 1 - check[x]

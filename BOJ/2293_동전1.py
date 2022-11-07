0945b959d2913deae36adff57030cb74ac9ef21d
choizlor <choizlor@gmail.com>
2022-11-07 23:58:13 +0900

    1107

--- BOJ/2293_동전1.py
+++ BOJ/2293_동전1.py
@@ -1,3 +1,12 @@
 n, k = map(int, input().split())
 coin = [int(input()) for _ in range(n)]
-coingggg
+
+dp = [0] * (k+1)
+dp[0] = 1
+
+for c in coin:
+    for i in range(c, k+1):
+        check = dp[i - c]
+        dp[i] += check
+
+print(dp[k])

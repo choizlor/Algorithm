2422e16c5861e9b042b7ad4df37081e2e10fa392
choizlor <choizlor@gmail.com>
2022-11-10 23:57:09 +0900

    1110

---
+++ BOJ/13913_숨바꼭질 4.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+def bfs():
+    dq = deque()
+    dq.append(N)
+    while dq:
+        num = dq.popleft()
+        for x in []
+
+
+N, K = map(int, input().split())
+minV = 100000000
+print(minV)

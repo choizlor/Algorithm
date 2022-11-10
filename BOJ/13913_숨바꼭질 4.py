e9dc443e1f75c1c5ae2de98521d6f1bbbe3d3dbd
choizlor <choizlor@gmail.com>
2022-11-11 00:31:35 +0900

    1110

--- BOJ/13913_숨바꼭질 4.py
+++ BOJ/13913_숨바꼭질 4.py
@@ -5,9 +5,25 @@ def bfs():
     dq.append(N)
     while dq:
         num = dq.popleft()
-        for x in []
+        if num == K:
+            return
+        for numx in [num-1, num+1, num*2]:
+            if 0<=numx<=100000 and visited[numx] == 0:
+                dq.append(numx)
+                visited[numx] = visited[num] + 1
+                route[numx] = num
 
 
 N, K = map(int, input().split())
-minV = 100000000
-print(minV)
+visited = [0] * 100001
+route = [0] * 100001
+bfs()
+
+result = []
+tmp = K
+for _ in range(visited[K]+1):
+    result.append(tmp)
+    tmp = route[tmp]
+
+print(visited[K])
+print(*result[::-1])

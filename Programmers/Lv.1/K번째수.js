3a849fe48d03a41be04f21dc53e7b55cbfc8a82f
choizlor <choizlor@gmail.com>
2023-03-22 09:02:04 +0900

    [PGS] K번째수 / lv.1 / 20분

---
+++ Programmers/Lv.1/K번째수.js
@@ -0,0 +1,20 @@
+function solution(array, commands) {
+  let answer = [];
+  for (let i = 0; i < commands.length; i++) {
+    k = commands[i][2] - 1;
+
+    arr = array.slice(commands[i][0] - 1 , commands[i][1]);
+    arr.sort((a,b)=>{ return a-b });
+    answer.push(arr[k]);
+  }
+  return answer;
+}
+
+solution(
+  [1, 5, 2, 6, 3, 7, 4],
+  [
+    [2, 5, 3],
+    [4, 4, 1],
+    [1, 7, 3],
+  ]
+);

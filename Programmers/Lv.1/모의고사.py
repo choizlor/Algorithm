c5f6c477b5ade54d09361ff583c935d4bf1c91f8
choizlor <choizlor@gmail.com>
2023-04-26 00:00:00 +0900

    [PGS] 모의고사 수정

--- Programmers/Lv.1/모의고사.py
+++ Programmers/Lv.1/모의고사.py
@@ -1,22 +1,21 @@
 def solution(answers):
     answer = [0 for i in range(3)]
-
-    man1 = [1,2,3,4,5]
-    man2 = [2,1,2,3,2,4,2,5]
-    man3 = [3,3,1,1,2,2,4,4,5,5]
     
-    for i in range(len(answers)):
-        ans = answers[i]
-        if(man1[i%len(man1)] == ans):
+    math1 = [1, 2, 3, 4, 5]
+    math2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    math3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
+    for a in range(len(answers)):
+        if answers[a] == math1[a % 5]:
             answer[0] += 1
-        if(man2[i%len(man2)] == ans):
+        if answers[a] == math2[a % 8]:
             answer[1] += 1
-        if(man3[i%len(man3)] == ans):
-            answer[2] += 1     
+        if answers[a] == math3[a % 10]:
+            answer[2] += 1
     
     result = []
-    for i in range(len(answer)):
-        if(answer[i] == max(answer)):
+    for i in range(3):
+        if answer[i] == max(answer):
             result.append(i+1)
     
     return sorted(result)

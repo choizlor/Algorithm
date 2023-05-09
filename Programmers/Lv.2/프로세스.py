3db796d7f43028c4bdc425359244b23940123872
choizlor <choizlor@gmail.com>
2023-05-10 01:05:54 +0900

    [PGS] 프로세스 / lv.2 / 30분

--- Programmers/Lv.2/프로세스.py
+++ Programmers/Lv.2/프로세스.py
@@ -1,3 +1,11 @@
 def solution(priorities, location):
+    queue = [(idx, p) for idx, p in enumerate(priorities)]
     answer = 0
-    return answer
+    while True:
+        tmp = queue.pop(0)
+        if any(tmp[1] < q[1] for q in queue):   # 하나라도 조건에 맞으면 True
+            queue.append(tmp)
+        else:
+            answer += 1
+            if tmp[0] == location:
+                return answer

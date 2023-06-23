bdb0bd0c8268e97cadca2edba9b5b5c5ccd55c4e
choizlor <choizlor@gmail.com>
2023-06-23 21:36:17 +0900

    [PGS] 영어 끝말잇기 / lv.2 / 수정

--- Programmers/Lv.2/영어 끝말잇기.py
+++ Programmers/Lv.2/영어 끝말잇기.py
@@ -1,16 +1,18 @@
 def solution(n, words):
-    used_words = []
-    number, order = 0, 0
+    user_num, word_num = 0, 0
+    check = {}
+    check[words[0]] = 1
 
-    used_words.append(words[0])
-    last_word = words[0][-1]
     for i in range(1, len(words)):
-        if words[i] not in used_words and words[i][0] == last_word:
-            used_words.append(words[i])
-            last_word = words[i][-1]
-        else:
-            number = (i % n) + 1
-            order = (i // n) + 1
-            break
+        user_num = (i % n) + 1
+        word_num = (i // n) + 1
 
-    return [number, order]
+        if words[i - 1][-1] != words[i][0]:
+            return [user_num, word_num]
+
+        if words[i] in check:
+            return [user_num, word_num]
+        else:
+            check[words[i]] = 1
+
+    return [0, 0]
